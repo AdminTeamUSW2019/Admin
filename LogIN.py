e9196f4b9cfdcf2778f09e81b57319d4374cf3b6
Bobby Granate <60476771+17045592@users.noreply.github.com>
2020-03-19 18:04:32 +0000

    Add files via upload

--- LogIN.py
+++ LogIN.py
@@ -4,6 +4,8 @@ from tkinter import ttk
 import sqlite3
 import webbrowser
 
+from datetime import datetime
+
 root = Tk()
 menu = Menu(root)
 
@@ -20,28 +22,33 @@ pw.pack(fill = BOTH, expand = True)
 
 USERNAME = StringVar()
 PASSWORD = StringVar()
+FNAME = StringVar()
 
 def ShowPolicy():
     webbrowser.open_new(r"https://github.com/AdminTeamUSW2019/Admin---GUI/blob/master/PolicyV1.md")
 
+##UNUSED
 def Database():
     global conn, cursor
-    conn = sqlite3.connect("pythontut.db")
+    conn = sqlite3.connect("GUITest.db")
     cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS `admin` (admin_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT, password TEXT)")
-    cursor.execute("CREATE TABLE IF NOT EXISTS `product` (product_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, product_name TEXT, product_qty TEXT, product_price TEXT)")
-    cursor.execute("SELECT * FROM `admin` WHERE `username` = 'admin' AND `password` = 'admin'")
+    now = datetime.now()
+    now = now.isoformat()
+    cursor.execute("CREATE TABLE IF NOT EXISTS `newloggedin` (username VARCHAR(30), password VARCHAR(30), activity STRING)")
+    ##Current Progress
+    cursor.execute("SELECT * FROM `users` WHERE `username` = 'users' AND `password` = 'users' AND 'activity' = ?", (now))
     if cursor.fetchone() is None:
-        cursor.execute("INSERT INTO `admin` (username, password) VALUES('admin', 'admin')")
+        cursor.execute("INSERT INTO `loggedin` (username, password) VALUES('users', 'users')")
         conn.commit()
+##UNUSED
 
 def ShowLoginForm():
     global loginform
     loginform = Toplevel()
     loginform.title("Account Login")
     loginform.configure(bg='grey')
-    width = 2000
-    height = 1000
+    width = 1000
+    height = 500
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
     x = (screen_width / 2) - (width / 2)
@@ -52,18 +59,22 @@ def ShowLoginForm():
 
 def LoginForm():
     global lbl_result
-    TopLoginForm = Frame(loginform, width=600, height=100, bd=1, relief=SOLID)
-    TopLoginForm.pack(side=TOP, pady=20)
-    lbl_text = Label(TopLoginForm, text="Administrator Login", font=('arial', 18), width=600)
+    TopLoginForm = Frame(loginform, width=600, height=100, relief=SOLID)
+    TopLoginForm.pack(side=TOP, pady=1)
+    lbl_text = Label(TopLoginForm, text="System Support Login", font=('arial', 18), width=600)
     lbl_text.pack(fill=X)
     MidLoginForm = Frame(loginform, width=600)
     MidLoginForm.pack(side=TOP, pady=50)
+    MidLoginForm.configure(bg='grey')
     lbl_username = Label(MidLoginForm, text="Username:", font=('arial', 25), bd=18)
     lbl_username.grid(row=0)
+    lbl_username.configure(bg='grey')
     lbl_password = Label(MidLoginForm, text="Password:", font=('arial', 25), bd=18)
     lbl_password.grid(row=1)
+    lbl_password.configure(bg='grey')
     lbl_result = Label(MidLoginForm, text="", font=('arial', 18))
     lbl_result.grid(row=3, columnspan=2)
+    lbl_result.configure(bg='grey')
     username = Entry(MidLoginForm, textvariable=USERNAME, font=('arial', 25), width=15)
     username.grid(row=0, column=1)
     password = Entry(MidLoginForm, textvariable=PASSWORD, font=('arial', 25), width=15, show="*")
@@ -73,20 +84,33 @@ def LoginForm():
     btn_login.bind('<Return>', Login)
 
 def Login(event=None):
-    global admin_id
-    Database()
+    ##Database()
+    global conn, cursor, cursor2, lname, username
+    conn = sqlite3.connect("GUITest.db")
+    cursor = conn.cursor()
+    cursor2 = conn.cursor()
     if USERNAME.get == "" or PASSWORD.get() == "":
         lbl_result.config(text="Please complete the required field!", fg="red")
     else:
-        cursor.execute("SELECT * FROM `admin` WHERE `username` = ? AND `password` = ?", (USERNAME.get(), PASSWORD.get()))
+        cursor.execute("SELECT * FROM `users` WHERE `username` = ? AND `password` = ?", (USERNAME.get(), PASSWORD.get()))
         if cursor.fetchone() is not None:
-            cursor.execute("SELECT * FROM `admin` WHERE `username` = ? AND `password` = ?", (USERNAME.get(), PASSWORD.get()))
-            data = cursor.fetchone()
-            admin_id = data[0]
-            USERNAME.set("")
-            PASSWORD.set("")
-            lbl_result.config(text="")
-            ShowHome()
+            cursor2.execute("SELECT * FROM `users` WHERE `username` = ? AND `password` = ? AND 'accesslevel' > 0", (USERNAME.get(), PASSWORD.get()))
+            if cursor2.fetchmany() is not None:
+                data = cursor2.fetchmany()
+                for rows in data:
+                    username = data[0]
+                    FNAME.set(data[3])
+                    lname = data[4]
+                USERNAME.set("")
+                PASSWORD.set("")
+                lbl_result.config(text="")
+                ShowAdminForm()
+            else:
+                data = cursor.fetchone()
+                USERNAME.set("")
+                PASSWORD.set("")
+                lbl_result.config(text="")
+                ShowPolicy()
         else:
             lbl_result.config(text="Invalid username or password", fg="red")
             USERNAME.set("")
@@ -138,17 +162,48 @@ def QueryForm():
 def ShowAdminForm():
     global adminform
     adminform = Toplevel()
-    adminform.title("Query Submission")
-    width = 2000
-    height = 1000
+    adminform.title("Admin Menu")
+    width = 1000
+    height = 500
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
     x = (screen_width / 2) - (width / 2)
     y = (screen_height / 2) - (height / 2)
     adminform.resizable(0, 0)
     adminform.geometry("%dx%d+%d+%d" % (width, height, x, y))
-    AdminForm()
+    NewAdminForm()
 
+def NewAdminForm():
+    lbl_message = 'Welcome ' + FNAME.get()
+    TopAForm = Frame(adminform, width=600, height=100, bd=0, relief=SOLID)
+    TopAForm.pack(side=TOP)
+    lbl_text = Label(TopAForm, text="Admin", font=('arial', 18), width=600, bg='grey', fg='white')
+    lbl_text.pack(fill=X)
+
+    style = ttk.Style(adminform)
+    style.configure('lefttab.TNotebook', tabposition='wn', background='grey')
+    notebook = ttk.Notebook(adminform, style='lefttab.TNotebook')
+    
+    f1 = tk.Frame(notebook, bg='white', width=1000, height=500)
+    lbl_dash = Label(f1, text=lbl_message, font=('arial', 25), bg='white')
+    lbl_dash.pack(fill=X)
+    
+    f2 = tk.Frame(notebook, bg='white', width=1000, height=500)
+    sb = Scrollbar(f2)
+    sb.pack(side = RIGHT, fill=Y)
+    mylist = Listbox(f2, yscrollcommand = sb.set)
+    for line in range(30):  
+        mylist.insert(END, "Number " + str(line))  
+    mylist.pack( fill=BOTH )  
+    sb.config( command = mylist.yview )  
+
+    f3 = tk.Frame(notebook, bg='green', width=1000, height=500)
+    notebook.add(f1, text=f'{"Dashboard": ^20s}')
+    notebook.add(f3, text=f'{"Queries": ^24s}')
+    notebook.add(f2, text=f'{"User Log": ^23s}')
+    notebook.pack(side=TOP)
+
+##Old
 def AdminForm():
     global lbl_result
     TopAForm = Frame(adminform, width=600, height=100, bd=0, relief=SOLID)
@@ -180,6 +235,26 @@ def AdminForm():
     btn_login = Button(MidQForm, text="Login", font=('arial', 18), width=30, command=Login)
     btn_login.grid(row=2, columnspan=2, pady=20)
     btn_login.bind('<Return>', Login)
+##Old
+
+def DatabaseTest():
+    conn = sqlite3.connect("GUITest.db")
+    crsr = conn.cursor()
+    sql_command = """CREATE TABLE users (
+    username VARCHAR(30) PRIMARY KEY, 
+    password VARCHAR(30),
+    accesslevel INT, 
+    fname VARCHAR(20),  
+    lname VARCHAR(30));"""
+
+    crsr.execute(sql_command)
+
+    sql_command = """INSERT INTO users VALUES ("UserOne", "password", 1, "Bob", "Jones");"""
+    crsr.execute(sql_command)
+    sql_command = """INSERT INTO users VALUES ("UserTwo", "passwordTwo", 0, "Bill", "Gates");"""
+    crsr.execute(sql_command)
+    conn.commit()
+    conn.close()
 
 def ShowHome():
     root.show()
@@ -189,6 +264,7 @@ nextmenu.add_command(label='Log In', command=ShowLoginForm)
 nextmenu.add_command(label='Query', command=ShowQueryForm)
 nextmenu.add_command(label='Admin', command=ShowAdminForm)
 nextmenu.add_command(label='Policy', command=ShowPolicy)
+nextmenu.add_command(label='Create Test Database', command=DatabaseTest)
 nextmenu.add_separator()
 nextmenu.add_command(label='Exit', command=root.quit)
 
